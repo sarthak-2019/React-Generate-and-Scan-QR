22eac4497c19323f5d236fc67b1bd75401255b29
Sarthak Shukla <sarthak2019fractal@gmail.com>
2022-02-07 22:21:49 +0530

    Generate and Scan a QR

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,3 @@
-import { Grid, Button, Typography } from "@material-ui/core";
-import Icon from "@mdi/react";
-import { mdiQrcode, mdiQrcodeScan } from "@mdi/js";
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -9,10 +6,14 @@ function Home() {
       <h1>Scan and Generate a QR</h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
         <Link to="/qr_generator">
-          <button style={{ padding: "20px" }}>Generate A QR</button>
+          <button style={{ padding: "20px", cursor: "pointer" }}>
+            Generate A QR
+          </button>
         </Link>
         <Link to="/qr_scanner">
-          <button style={{ padding: "20px" }}>Scan A QR</button>
+          <button style={{ padding: "20px", cursor: "pointer" }}>
+            Scan A QR
+          </button>
         </Link>
       </div>
     </div>
--- src/pages/QRscanner.js
+++ src/pages/QRscanner.js
@@ -1,51 +1,31 @@
-import React, {useState} from 'react'
-import {Fab, TextareaAutosize} from '@material-ui/core'
-import {ArrowBack} from '@material-ui/icons'
-import { Link } from "react-router-dom";
-import QrScan from 'react-qr-reader'
+import React, { useState } from "react";
+import QrScan from "react-qr-reader";
 
 function QRscanner() {
-
-    const [qrscan, setQrscan] = useState('No result');
-    const handleScan = data => {
-        if (data) {
-            setQrscan(data)
-        }
+  const [qrscan, setQrscan] = useState("No result");
+  const handleScan = (data) => {
+    if (data) {
+      setQrscan(data);
+      window.location.href = data;
     }
-    const handleError = err => {
-    console.error(err)
-    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
 
-    return (
-      <div>
-            <Link to="/">
-            <Fab style={{marginRight:10}} color="primary">
-                <ArrowBack/>
-            </Fab>
-            </Link>
-            <span>QR Scanner</span>
-            
-            <center>
-            <div style={{marginTop:30}}>
-                <QrScan
-                    delay={300}
-                    onError={handleError}
-                    onScan={handleScan}
-                    style={{ height: 240, width: 320 }}
-                />
-            </div>
-            </center>
-
-            <TextareaAutosize
-                style={{fontSize:18, width:320, height:100, marginTop:100}}
-                rowsMax={4}
-                defaultValue={qrscan}
-                value={qrscan}
-            />
-
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+      <span>QR Scanner</span>
+      <div style={{ marginTop: 30 }}>
+        <QrScan
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ height: 240, width: 320 }}
+        />
       </div>
-    );
-  }
-  
-  export default QRscanner;
-  
+    </div>
+  );
+}
+
+export default QRscanner;
